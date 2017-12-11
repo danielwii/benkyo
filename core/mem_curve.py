314e62e645fb0126d0138eecfeafc849f982db43
Daniel Wei <danielwii0326@gmail.com>
2017-12-11 22:55:54 +0800

    update algorithm and layout

--- core/mem_curve.py
+++ core/mem_curve.py
@@ -62,25 +62,21 @@ def calc_ranks(ranks: int, choice: int, first: bool = False) -> tuple:
     if choice not in (0, 1, 2, 3, 4):
         raise ValueError("choice must be in 0,1,2,3,4")
 
-    # if first and choice not in (0, 2, 4):
-    #     raise ValueError("choice must be in 0,2,4 while it's the first")
-
-    if not first and choice not in (1, 2, 3):
-        raise ValueError("choice must be in 1,2,3 while not the first")
-
     if first:
         _ranks = {
             0: lambda x: 0,
             1: lambda x: 10,
-            2: lambda x: 40,
-            3: lambda x: 50,
+            2: lambda x: 30,
+            3: lambda x: 45,
             4: lambda x: 60,
         }[choice](ranks)
     else:
         _ranks = {
-            1: lambda x: x - 5,
-            2: lambda x: x,
+            0: lambda x: 0,
+            1: lambda x: x - 10,
+            2: lambda x: x - 5,
             3: lambda x: x + 5,
+            4: lambda x: 60,
         }[choice](ranks)
 
     if _ranks < 0:
