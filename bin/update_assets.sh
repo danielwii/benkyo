07ad6d2a832cc6cf2d764f4583cf3dfe18e3ef80
Daniel Wei <danielwii0326@gmail.com>
2017-12-14 03:36:03 +0800

    fix shell cp issue

--- bin/update_assets.sh
+++ bin/update_assets.sh
@@ -14,6 +14,6 @@ mkdir -p ui/static/fonts
 cp -f node_modules/bulma/css/bulma.css ui/static/libs/bulma.css
 cp -f node_modules/font-awesome/css/font-awesome.css ui/static/libs/font-awesome.css
 
-cp -rf node_modules/font-awesome/fonts/ ui/static/fonts/
+cp -rf node_modules/font-awesome/fonts/* ui/static/fonts/
 
 echo '[x] ^_^ done!'
